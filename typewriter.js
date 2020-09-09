9c791da80306ed16cb5a27f9cdab81a1621c007a
Nikoline <niko702j@stud.kea.dk>
2020-09-09 10:46:08 +0200

    typewriter works

---
+++ typewriter.js
@@ -0,0 +1,31 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", start);
+
+let type = document.querySelector("#typewriter");
+const text = type.textContent;
+const textLen = text.length;
+let count = 0;
+
+function start() {
+  type.textContent = "";
+  count = 0;
+
+  write();
+}
+
+function write() {
+  console.log(text);
+
+  if (textLen == count) {
+    setTimeout(start, 1000);
+  } else {
+    addType();
+  }
+}
+
+function addType() {
+  type.textContent = text.substring(0, count + 1);
+  count++;
+  setTimeout(write, 200);
+}
